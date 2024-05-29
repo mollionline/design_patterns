35aef04390bdd94f25e59888d29a6b57db6de316
moldira1004 <moldir.auezova>
2024-05-29 11:30:37 +0500

    solid

---
+++ solid/single_responsibility_principle.py
@@ -0,0 +1,49 @@
+# SRP SOC
+class Journal:
+    def __init__(self):
+        self.entries = []
+        self.count = 0
+
+    def add_entry(self, text):
+        self.count += 1
+        self.entries.append(f'{self.count}: {text}')
+
+    def remove_entry(self, pos):
+        del self.entries[pos]
+
+    def __str__(self):
+        return '\n'.join(self.entries)
+
+    # def save(self, filename):
+    #     file = open(filename, 'w')
+    #     file.write(str(self))
+    #     file.close()
+    #
+    # def load(self, filename):
+    #     pass
+    #
+    # def low_from_web(self, uri):
+    #     pass
+
+
+""" YOU shouldn't put too many responsibility to one class """
+
+
+class PersistenceManager:
+    @staticmethod
+    def save_to_file(journal, filename):
+        file = open(filename, 'w')
+        file.write(str(journal))
+        file.close()
+
+
+j = Journal()
+j.add_entry('I cried today')
+j.add_entry('I ate a bug')
+
+file = './journal.txt'
+PersistenceManager.save_to_file(j, file)
+print(f'Journal entries : \n{j}')
+
+with open(file) as f:
+    print(f.read())
